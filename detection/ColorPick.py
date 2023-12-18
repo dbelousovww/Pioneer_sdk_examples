ace1a051698f76a47550ac369fe0af2321748c86
dbelousovww <70803617+dbelousovww@users.noreply.github.com>
2023-12-18 18:03:47 +0300

    Update ColorPick.py
    добавил комменты на русском

--- detection/ColorPick.py
+++ detection/ColorPick.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
-import json
 from pioneer_sdk import Camera
 
+# Ширина и высота кадра
 frameWidth = 480
 frameHeight = 360
 camera = Camera()
 
-
+# Функция-заглушка для создания трекбаров
 def empty(a):
     pass
 
-
+# Создание окна HSV и трекбаров для настройки параметров
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV", 640, 240)
 cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
@@ -21,13 +21,16 @@ cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
 cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
 cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
+# Имя файла для сохранения значений HSV
 filename = "hsv_values.txt"
 
 while True:
+    # Получение кадра с камеры и изменение размера
     img = camera.get_cv_frame()
     img = cv2.resize(img, (frameWidth, frameHeight))
     imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    # Получение значений трекбаров
     h_min = cv2.getTrackbarPos("HUE Min", "HSV")
     h_max = cv2.getTrackbarPos("HUE Max", "HSV")
     s_min = cv2.getTrackbarPos("SAT Min", "HSV")
@@ -35,19 +38,24 @@ while True:
     v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
     v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
 
-    # Write values to a text file
+    # Запись значений в текстовый файл
     with open(filename, 'w') as txt_file:
         txt_file.write(f"lower [{h_min}, {s_min}, {v_min}] \nupper [{h_max}, {s_max}, {v_max}]")
 
+    # Определение нижней и верхней границ маски
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
 
+    # Создание маски и применение ее к изображению
     mask = cv2.inRange(imgHsv, lower, upper)
     result = cv2.bitwise_and(img, img, mask=mask)
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    hStack = np.hstack([img, mask, result])
-    cv2.imshow('Horizontal Stacking', hStack)
 
+    # Горизонтальное объединение изображения, маски и результата
+    hStack = np.hstack([img, mask, result])
+    cv2.imshow('Горизонтальное объединение', hStack)
+
+    # Выход из цикла при нажатии клавиши 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
