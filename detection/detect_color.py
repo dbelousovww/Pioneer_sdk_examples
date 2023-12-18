51a4edf501bc68fbc580a874d189b545ae710a04
dbelousovww <70803617+dbelousovww@users.noreply.github.com>
2023-12-18 18:04:52 +0300

    Update detect_color.py
    комменты на русском

--- detection/detect_color.py
+++ detection/detect_color.py
@@ -2,40 +2,40 @@ import cv2
 import numpy as np
 from pioneer_sdk import Camera
 
-# Connect to the drone camera
+# Подключение к камере дрона
 camera = Camera()
 
 def filter_red_color(frame):
-    # Convert the frame to the HSV color space
+    # Преобразование кадра в цветовое пространство HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Define the lower and upper bounds for the red color in HSV
+    # Определение диапазона красного цвета в HSV
     lower_red = np.array([0, 110, 120])
     upper_red = np.array([10, 255, 255])
 
-    # Create a mask for the red color
+    # Создание маски для красного цвета
     red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
 
-    # Apply the mask to the original frame
+    # Применение маски к исходному кадру
     red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
 
     return red_objects
 
 if __name__ == "__main__":
     while True:
-        frame = camera.get_cv_frame()  # Get frame (in this case already decoded)
+        frame = camera.get_cv_frame()  # Получение кадра (в данном случае уже декодированного)
 
-        # Resize the frame to a specific width and height
-        new_width = 640  # specify the new width
-        new_height = 480  # specify the new height
+        # Изменение размера кадра до определенной ширины и высоты
+        new_width = 640  # указать новую ширину
+        new_height = 480  # указать новую высоту
         frame = cv2.resize(frame, (new_width, new_height))
 
-        # Filter red color
+        # Фильтрация красного цвета
         red_objects = filter_red_color(frame)
 
-        cv2.imshow("video", red_objects)  # Show the frame with red objects on the screen
+        cv2.imshow("video", red_objects)  # Показать кадр с красными объектами на экране
 
-        if cv2.waitKey(1) == 27:  # Exit if the ESC key is pressed
+        if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
             break
 
-    cv2.destroyAllWindows()  # Close all opened OpenCV windows
+    cv2.destroyAllWindows()  # Закрыть все открытые окна OpenCV
