51d1b1e9115db6d7a0370bb156a052a68cf1c545
dbelousovww <70803617+dbelousovww@users.noreply.github.com>
2023-12-18 18:04:07 +0300

    Update contour_find_center.py
    комменты на русском

--- detection/contour_find_center.py
+++ detection/contour_find_center.py
@@ -2,34 +2,34 @@ import cv2
 import numpy as np
 from pioneer_sdk import Camera
 
-# Connect to the drone camera
+# Подключение к камере дрона
 camera = Camera()
 
 def filter_red_color(frame):
-    # Convert the frame to the HSV color space
+    # Преобразование кадра в цветовое пространство HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Define the lower and upper bounds for the red color in HSV
+    # Определение диапазона красного цвета в HSV
     lower_red = np.array([0, 100, 110])
     upper_red = np.array([10, 255, 255])
 
-    # Create a mask for the red color
+    # Создание маски для красного цвета
     red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
 
-    # Apply the mask to the original frame
+    # Применение маски к исходному кадру
     red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
 
     return red_objects, red_mask
 
 def find_object_center(mask):
-    # Find contours on the mask
+    # Поиск контуров на маске
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        # Find the contour with the maximum area
+        # Поиск контура с максимальной площадью
         max_contour = max(contours, key=cv2.contourArea)
 
-        # Calculate the center of mass of the contour
+        # Вычисление центра масс контура
         M = cv2.moments(max_contour)
         if M["m00"] != 0:
             center_x = int(M["m10"] / M["m00"])
@@ -39,26 +39,26 @@ def find_object_center(mask):
 
 if __name__ == "__main__":
     while True:
-        frame = camera.get_cv_frame()  # Get frame (in this case already decoded)
+        frame = camera.get_cv_frame()  # Получение кадра (в данном случае уже декодированного)
 
-        # Resize the frame to a specific width and height
-        new_width = 640  # specify the new width
-        new_height = 480  # specify the new height
+        # Изменение размера кадра до определенной ширины и высоты
+        new_width = 640  # указать новую ширину
+        new_height = 480  # указать новую высоту
         frame = cv2.resize(frame, (new_width, new_height))
 
-        # Filter red color
+        # Фильтрация красного цвета
         red_objects, red_mask = filter_red_color(frame)
 
-        # Find the center of the red object
+        # Поиск центра красного объекта
         red_center = find_object_center(red_mask)
 
         if red_center:
-            # Draw a circle at the center of the red object
+            # Нарисовать круг в центре красного объекта
             cv2.circle(red_objects, red_center, 5, (0, 0, 255), -1)
 
-        cv2.imshow("video", red_objects)  # Show the frame with red object and center on the screen
+        cv2.imshow("video", red_objects)  # Показать кадр с красным объектом и центром на экране
 
-        if cv2.waitKey(1) == 27:  # Exit if the ESC key is pressed
+        if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
             break
 
-    cv2.destroyAllWindows()  # Close all opened OpenCV windows
+    cv2.destroyAllWindows()  # Закрыть все открытые окна OpenCV
