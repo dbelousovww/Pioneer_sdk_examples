95fb4fbce377653c6eba239c36a39c39647222c7
dbelousovww <70803617+dbelousovww@users.noreply.github.com>
2024-02-02 13:32:49 +0300

    Update contour_find_center.py
    проверка на нан

--- detection/contour_find_center.py
+++ detection/contour_find_center.py
@@ -40,25 +40,26 @@ def find_object_center(mask):
 if __name__ == "__main__":
     while True:
         frame = camera.get_cv_frame()  # Получение кадра (в данном случае уже декодированного)
-
-        # Изменение размера кадра до определенной ширины и высоты
-        new_width = 640  # указать новую ширину
-        new_height = 480  # указать новую высоту
-        frame = cv2.resize(frame, (new_width, new_height))
-
-        # Фильтрация красного цвета
-        red_objects, red_mask = filter_red_color(frame)
-
-        # Поиск центра красного объекта
-        red_center = find_object_center(red_mask)
-
-        if red_center:
-            # Нарисовать круг в центре красного объекта
-            cv2.circle(red_objects, red_center, 5, (0, 0, 255), -1)
-
-        cv2.imshow("video", red_objects)  # Показать кадр с красным объектом и центром на экране
-
-        if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
-            break
+        
+        if frame is not None:
+            # Изменение размера кадра до определенной ширины и высоты
+            new_width = 640  # указать новую ширину
+            new_height = 480  # указать новую высоту
+            frame = cv2.resize(frame, (new_width, new_height))
+    
+            # Фильтрация красного цвета
+            red_objects, red_mask = filter_red_color(frame)
+    
+            # Поиск центра красного объекта
+            red_center = find_object_center(red_mask)
+    
+            if red_center:
+                # Нарисовать круг в центре красного объекта
+                cv2.circle(red_objects, red_center, 5, (0, 0, 255), -1)
+    
+            cv2.imshow("video", red_objects)  # Показать кадр с красным объектом и центром на экране
+    
+            if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
+                break
 
     cv2.destroyAllWindows()  # Закрыть все открытые окна OpenCV
