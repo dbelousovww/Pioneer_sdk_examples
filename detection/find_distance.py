fb1e35f1c297103ed9053b8bb3d903a93140decf
dbelousovww <70803617+dbelousovww@users.noreply.github.com>
2024-02-02 13:34:19 +0300

    Update find_distance.py

--- detection/find_distance.py
+++ detection/find_distance.py
@@ -20,36 +20,37 @@ camera = Camera()
 while True:
     frame = camera.get_cv_frame()
 
-    # Фильтрация красного цвета
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0, 100, 110])
-    upper_red = np.array([10, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
-    red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
-
-    # Поиск контура красного объекта
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if contours:
-        # Находим контур с максимальной площадью
-        max_contour = max(contours, key=cv2.contourArea)
-
-        # Измеряем площадь контура
-        contour_area = cv2.contourArea(max_contour)
-
-        # Рассчитываем расстояние до объекта
-        radius = np.sqrt(contour_area / np.pi)
-        distance_to_object = (camera_matrix[0, 0] * size_of_object_real) / (2 * radius)
-
-        print(f"Расстояние до объекта: {distance_to_object:.2f} метров")
-
-        # Отрисовываем контур и выводим расстояние на экран
-        cv2.drawContours(red_objects, [max_contour], -1, (0, 255, 0), 2)
-        cv2.putText(red_objects, f"Distance: {distance_to_object:.2f} m", (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-    cv2.imshow("video", red_objects)
-
-    if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
-        break
+    if frame is not None:
+        # Фильтрация красного цвета
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_red = np.array([0, 100, 110])
+        upper_red = np.array([10, 255, 255])
+        red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+        red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
+    
+        # Поиск контура красного объекта
+        contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            # Находим контур с максимальной площадью
+            max_contour = max(contours, key=cv2.contourArea)
+    
+            # Измеряем площадь контура
+            contour_area = cv2.contourArea(max_contour)
+    
+            # Рассчитываем расстояние до объекта
+            radius = np.sqrt(contour_area / np.pi)
+            distance_to_object = (camera_matrix[0, 0] * size_of_object_real) / (2 * radius)
+    
+            print(f"Расстояние до объекта: {distance_to_object:.2f} метров")
+    
+            # Отрисовываем контур и выводим расстояние на экран
+            cv2.drawContours(red_objects, [max_contour], -1, (0, 255, 0), 2)
+            cv2.putText(red_objects, f"Distance: {distance_to_object:.2f} m", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    
+        cv2.imshow("video", red_objects)
+    
+        if cv2.waitKey(1) == 27:  # Выход при нажатии клавиши ESC
+            break
 
 cv2.destroyAllWindows()
